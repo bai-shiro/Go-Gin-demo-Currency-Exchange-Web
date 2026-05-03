09764fc68e56aa6bd9eb35fe0cac55eaaf213d60
bai-shiro <bai.shiro@outlook.com>
2026-05-03 17:02:27 +0800

    v0.03-汇率兑换数据功能实现

---
+++ Exchangeapp_backend/controllers/exchange_rate_controller.go
@@ -0,0 +1,43 @@
+package controllers
+
+import (
+	"exchangeapp/global"
+	"exchangeapp/models"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateExchangeRate(ctx *gin.Context) {
+	var exchangeRate models.ExchangeRate
+
+	if err := ctx.ShouldBindJSON(&exchangeRate); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"Error" : err.Error()})
+		return
+	}
+
+	exchangeRate.Data = time.Now()
+
+	if err := global.Db.AutoMigrate(&exchangeRate); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"Error" : err.Error()})
+		return
+	}
+
+	if err := global.Db.Create(&exchangeRate).Error; err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"Error" : err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, exchangeRate)
+}
+
+func GetExchangeRates(ctx *gin.Context) {
+	var exchangeRates []models.ExchangeRate
+
+	if err := global.Db.Find(&exchangeRates).Error; err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"Error" : err.Error()})
+		return
+	}
+	ctx.JSON(http.StatusOK, exchangeRates)
+}
